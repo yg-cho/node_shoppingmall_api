08f6e72e1b2e4199564ad3c084bdc95d243285db
ygcho <sorisori01@naver.com>
2020-06-22 19:35:27 +0900

    request response test

--- server.js
+++ server.js
@@ -3,8 +3,20 @@
 const express = require("express");
 const app = express();
 
+app.use((req,res) => {
+    res.json({
+        message: 'It works!'
+    });
+});
+
+
+
+
+
+
+
+
 
 
 const PORT = 1111;
-
 app.listen(PORT, console.log("server started"));
