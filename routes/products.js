bed39ec738c8cfaa55553aa4eee9f7ae43a3b32c
ygcho <sorisori01@naver.com>
2020-06-22 19:55:58 +0900

    add product router

---
+++ routes/products.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+
+
+// product 정보 불러오기
+router.get('/', (req, res) => {
+    res.json({
+        message: 'product total get'
+    });
+});
+
+// product 정보 등록하기
+router.post('/', (req, res) => {
+    res.json({
+        message: 'product post'
+    });
+});
+
+// product 정보 수정하기
+
+router.put('/', (req, res) => {
+    res.json({
+        message: 'product put'
+    });
+});
+
+
+
+// product 정보 삭제하기
+
+router.delete('/', (req, res) => {
+    res.json({
+        message: 'product delete'
+    });
+});
+
+
+
+
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,20 +3,20 @@
 const express = require("express");
 const app = express();
 
-app.use((req,res) => {
-    res.json({
-        message: 'It works!'
-    });
-});
+// app.use((req,res) => {
+//     res.json({
+//         message: 'It works!'
+//     });
+// });
+
+
+const productRoute = require("./routes/products");
+
+
+app.use("/product", productRoute);
 
 
 
 
-
-
-
-
-
-
-const PORT = 1111;
+const PORT = 3000;
 app.listen(PORT, console.log("server started"));
