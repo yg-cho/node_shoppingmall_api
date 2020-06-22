9e221c214194155d280e5ffe820304aed0597ff8
ygcho <sorisori01@naver.com>
2020-06-22 21:08:27 +0900

    add product model

---
+++ models/products.js
@@ -0,0 +1,13 @@
+
+
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+    name : String,
+    price : Number
+    
+});
+
+
+
+module.exports = mongoose.model("product", productSchema);
