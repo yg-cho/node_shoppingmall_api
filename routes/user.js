0bf46091f529308bb2f4bbc0240236eb01ee8195
ygcho <sorisori01@naver.com>
2020-07-03 20:08:23 +0900

    email validatioin

--- routes/user.js
+++ routes/user.js
@@ -7,35 +7,53 @@ const userModel = require('../models/user')
 //회원가입
 router.post('/register', (req, res) => {
 
-    // password 암호화
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if(err) {
-            return res.status(500).json({
-                error: err
-            });
-        } else {
-
-            // 암호화 성공시 database에 저장
-            const user = new userModel({
-                username: req.body.username,
-                email: req.body.email,
-                password: hash
-            });
-            user
-                .save()
-                .then(user => {
-                    res.json({
-                        message: "Registered OK",
-                        userInfo: user
-                    });
-                })
-                .catch(err => {
-                    res.json({
-                        error: err.message
-                    });
+    // 등록여부(email) 유무체크
+    userModel
+        .findOne({ email: req.body.email })
+        //.findOne({ username: req.body.username })
+        .then(user => {
+            if(user) {
+                return res.json({
+                    message : "email already used"
                 });
-        }
-    })
+            } else {
+                // password 암호화
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if(err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+
+                        // 암호화 성공시 database에 저장
+                        const user = new userModel({
+                            username: req.body.username,
+                            email: req.body.email,
+                            password: hash
+                        });
+                        user
+                            .save()
+                            .then(user => {
+                                res.json({
+                                    message: "Registered OK",
+                                    userInfo: user
+                                });
+                            })
+                            .catch(err => {
+                                res.json({
+                                    error: err.message
+                                });
+                            });
+                    }
+                })
+            }
+        })
+        .catch(err => {
+            res.json({
+                error: err.message
+            });
+        });
+
 
 
 
