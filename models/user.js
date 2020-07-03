bfad32b416d9f7cbc22bd889e2503ee556de9784
ygcho <sorisori01@naver.com>
2020-07-03 20:13:34 +0900

    add email regex

--- models/user.js
+++ models/user.js
@@ -10,7 +10,10 @@ const userSchema = mongoose.Schema(
         },
         email : {
             type: String,
-            required: true
+            required: true,
+            unique: true,
+            match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
         },
         password : {
             type: String,
